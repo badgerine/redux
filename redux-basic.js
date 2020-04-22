3fc857ac63e1cc1b6042b37c6c4797cb195a3fad
sibusiso matsini <sbu.matsini@gmail.com>
2020-04-22 23:06:27 +0200

    S13#256 REDUX| dispatching actions

--- redux-basic.js
+++ redux-basic.js
@@ -7,12 +7,30 @@ const initialState = {
 
 //reducer
 const rootReducer = (state = initialState, action) => {
+    if(action.type === 'INC_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter +1
+        }
+    }
+    if(action.type === 'ADD_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+
     return state;
 }
 
 //store
 const store =createStore(rootReducer);
-console.log(store.getState());
+// console.log(store.getState());
 
 //dispatching action
+store.dispatch({type: 'INC_COUNTER'});
+console.log(store.getState());
+store.dispatch({type: 'ADD_COUNTER', value: 12});
+console.log(store.getState());
 //subscription
+
