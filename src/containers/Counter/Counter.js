ddcf54f2bf2fd289bbf55b621b6c041163a03eb8
sibusiso matsini <sbu.matsini@gmail.com>
2020-04-28 21:33:35 +0200

    S14#260-Assignment4 dispatching actions

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -31,9 +31,9 @@ class Counter extends Component {
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')} />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add', 5)} />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub', 5)} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)} />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)} />
             </div>
         );
     }
@@ -49,7 +49,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = dispatch => {
     console.log('[Counter:mapDispatchToProps] ... ');
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: (amount) => dispatch({type: 'ADD', value: amount}),
+        onSubtractCounter: (amount) => dispatch({type: 'SUBTRACT', value: amount}),
     };
 };
 
