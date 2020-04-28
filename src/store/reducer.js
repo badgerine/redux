4a4cdb5b07456fd6632701964d57124ba00e5695
sibusiso matsini <sbu.matsini@gmail.com>
2020-04-28 22:22:33 +0200

    S14#262 switch-case in reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,28 +4,18 @@ const initialState = {
     counter: 0
 };
 
-const reducer = (state = initialState, action) =>  {
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    if(action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if(action.type === 'ADD') {
-        return {
-            counter: state.counter + action.value
-        }
-    }
-    if(action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.value
-        }
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { counter: state.counter + 1 }
+        case 'DECREMENT':
+            return { counter: state.counter - 1 }
+        case 'ADD':
+            return { counter: state.counter + action.value }
+        case 'SUBTRACT':
+            return { counter: state.counter - action.value }
     }
     return state;
-}
+};
 
-export default reducer ;
+export default reducer;
